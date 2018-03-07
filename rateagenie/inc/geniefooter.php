8aeb36f186c736d42781919d2910a5819e42e5e4
Svetlana712 <shkarlat712@gmail.com>
2018-03-07 02:29:33 +0200

    Add files via upload

--- rateagenie/inc/geniefooter.php
+++ rateagenie/inc/geniefooter.php
@@ -4,36 +4,36 @@
             <h3>Our Services</h3>
             <ul>
                 <li><a href="dashcust.php" title="">Customer dashboard</a></li>
-                <li><a href="dashgenie.php"title="">Genie dashboard</a></li>
-                <li><a href="findjob.php" rel="nofollow" title="">Search jobs</a></li>
+                <li><a href="dashgenie.php" title="">Genie dashboard</a></li>
+                <li><a href="findjob.php" title="">Search jobs</a></li>
                 <li><a href="profile.php" title="">Edit profile</a></li>
-                <li><a href="repair.php" rel="nofollow" title="">Post a job</a></li>
-                <li><a href="jobcustomer.php" rel="nofollow" title="">Customer view on job</a></li>
-                <li><a href="contact.php" rel="nofollow" title="">Contact us</a></li>
+                <li><a href="repair.php" title="">Post a job</a></li>
+                <li><a href="jobcustomer.php" title="">Customer view on job</a></li>
+                <li><a href="contact.php" title="">Contact us</a></li>
             </ul>
         </div>
         <div class="footer_nav">
             <h3>Customers</h3>
             <ul>
                 <li><a href="faqscustomer.php" title="">Customer FAQs</a></li>
-                <li><a href="#"title="">Link Rate a genie</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
-                <li><a href="#" title="">Link</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
-                <li><a href="#" rel="nofollow" title="">Rate a genie</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
+                <li><a href="login.php" title="">Login</a></li>
+                <li><a href="register.php" title="">Register</a></li>
+                <li><a href="reset.php" title="">Reset password</a></li>
+                <li><a href="passv.php" title="">Set new password</a></li>
+                <li><a href="activejob-cust.php" title="">Active job - customer view</a></li>
+                <li><a href="#" title="">Link Rate a genie</a></li>
             </ul>
         </div>
         <div class="footer_nav last">
             <h3>Genies</h3>
             <ul>
                 <li><a href="faqsgenie.php" title="">Genies FAQs</a></li>
-                <li><a href="jobgenie.php"title="">Genie view on job</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
-                <li><a href="#" title="">Link</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
-                <li><a href="#" rel="nofollow" title="">Rate a genie</a></li>
-                <li><a href="#" rel="nofollow" title="">Link Rate a genie</a></li>
+                <li><a href="jobgenie.php" title="">Genie view on job</a></li>
+                <li><a href="jobgenie-not-applied.php" title="">Genie view on job - not applied</a></li>
+                <li><a href="view-prof-cust.php" title="">View customer profile by genie</a></li>
+                <li><a href="view-prof-genie.php" title="">View genie profile by customer</a></li>
+                <li><a href="messages.php" title="">Chat</a></li>
+                <li><a href="#" title="">Link Rate a genie</a></li>
             </ul>
         </div>
         <div class="footer_right">
@@ -51,7 +51,7 @@
                 <li><a href="#">Privacy Policy</a></li>
                 <li><a href="#">Terms of Use</a></li>
                 <li><a href="#">Sitemap</a></li>
-                <li><a rel="nofollow" href="#" target="_blank">Blog</a></li>
+                <li><a href="#" target="_blank">Blog</a></li>
                 <li class="last"><a href="#">RSS Feed</a></li>
             </ul>
             <p>Copyright 2018 Rate a Genie. All rights reserved.</p>
